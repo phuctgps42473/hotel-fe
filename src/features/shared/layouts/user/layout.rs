88db63861822e948ee9b23bcef8b05488e5e08ee
ps42473 <phuctgps42473@gmail.com>
2025-06-23 22:16:27 +0700

    remove old search bar and update some layouts

--- src/features/shared/layouts/user/layout.rs
+++ src/features/shared/layouts/user/layout.rs
@@ -1,16 +1,12 @@
 use leptos::prelude::*;
 
-use crate::features::{
-    home::components::search_bar::SearchBar,
-    shared::layouts::user::{footer::Footer, navbar::Navbar},
-};
+use crate::features::shared::layouts::user::{footer::Footer, navbar::Navbar};
 
 #[component]
 pub fn Layout(children: Children) -> impl IntoView {
     view! {
           <header class="block w-full fixed bottom-0 left-0 md:relative">
             <Navbar />
-            <SearchBar />
           </header>
           {children()}
           <Footer />
