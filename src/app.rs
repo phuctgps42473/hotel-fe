9da27409657f01386ca90d77c5e71f70ae36e5b2
ps42473 <phuctgps42473@gmail.com>
2025-06-29 17:29:16 +0700

    add reactive_stores crate for handling global states

--- src/app.rs
+++ src/app.rs
@@ -10,14 +10,30 @@ use leptoaster::*;
 use leptos::prelude::*;
 use leptos_meta::*;
 use leptos_router::{
-    components::{ParentRoute, Route, Router, Routes},
+    components::{Route, Router, Routes},
     path, StaticSegment,
 };
+use reactive_stores::Store;
+
+
+#[derive(Clone, Debug, Default, Store)]
+pub struct UserState {
+    email: Option<String>,
+    fullname: Option<String>,
+    id: Option<u32>,
+    role: Option<String>,
+}
+
+#[derive(Clone, Debug, Default, Store)]
+struct GlobalState {
+    user: UserState,
+}
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_toaster();
     provide_meta_context();
+    provide_context(Store::new(GlobalState::default()));
 
     view! {
     // <Stylesheet id="leptos" href="/style/output.css"/>
