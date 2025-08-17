d2d7bcde67aade08e9d6caeba6194822c36f1423
ps42473 <phuctgps42473@gmail.com>
2025-08-17 15:14:27 +0700

    make a global modal

--- src/features/shared/components/modal.rs
+++ src/features/shared/components/modal.rs
@@ -1,33 +1,29 @@
-use std::sync::Arc;
-
 use leptos::{portal::Portal, prelude::*};
 
 #[component]
-pub fn Modal(
-    hide_modal_fn: impl Fn() -> () + Send + Sync + Clone + 'static,
-    children: ChildrenFn,
-) -> impl IntoView {
-    let hide_modal_fn = Arc::new(hide_modal_fn);
-
+pub fn Modal<C>(
+    show_modal: ReadSignal<bool>,
+    set_show_modal: WriteSignal<bool>,
+    children: TypedChildrenFn<C>,
+) -> impl IntoView
+where
+    C: IntoView + 'static,
+{
+    let children = children.into_inner();
     view! {
         <Portal>
-          <div
-            on:click={
-                let hide_modal_fn = hide_modal_fn.clone();
-                move |_| {
-                    leptos::logging::log!("Clicked overlay");
-                    (hide_modal_fn)();
-                }
-            }
-            class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-          >
-            <div
-              on:click=move |ev|  ev.stop_propagation()
-              class="relative p-4 w-full max-w-md mx-auto max-h-full"
-            >
-              {children()}
-            </div>
-          </div>
+            <dialog class="modal" class:modal-open={show_modal}>
+                <div class="modal-box w-11/12 max-w-3xl">
+                        {children()}
+                    <div class="modal-action mt-6">
+                        <button class="btn rounded-[var(--radius-box)]" on:click=move |_| set_show_modal.set(false)>Đóng</button>
+                    </div>
+                </div>
+
+                <form method="dialog" class="modal-backdrop">
+                    <button on:click=move |_| set_show_modal.set(false)>close</button>
+                </form>
+            </dialog>
         </Portal>
     }
 }
